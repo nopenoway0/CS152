a3ec2d23ed821a758199cba610459b9356d1e04a
benj-G <benjamin.gottheil@sjsu.edu>
2017-02-09 01:02:17 -0800

    finished calculator

--- control_lab/Calculator.scala
+++ control_lab/Calculator.scala
@@ -2,39 +2,41 @@ object Calculator {
 
    // = 1 * 2 * 3 * ... * n
    def fact(n: Integer): Int = {
-    var base_fact = n;
-    var sum = 1;
+    var base_fact = n
+    var sum = 1
     while(base_fact > 1){
-      sum = sum * base_fact;
-      base_fact = base_fact - 1;
+      sum = sum * base_fact
+      base_fact = base_fact - 1
     }
-    return sum;
+    return sum
     }
    
    // = 1 + 2 + 3 + ... + n
    def tri(n: Integer): Int = {
     if(n > 1){
-      return n + tri(n - 1);
+      return n + tri(n - 1)
     }
-    else return 1;
+    else return 1
    }
    
    // = 2^n
    def exp(n: Integer): Int = {
-      val base = 2;
-      var product = 1;
-      var x = 0;
-      for(a <- 1 to n) product = product * base;
-      return product;
+      val base = 2
+      var product = 1
+      var x = 0
+      for(a <- 1 to n) product = product * base
+      return product
     }
    
    // = true if n >= 2 and has no smaller divisors
-   def isPrime(n: Integer): Bool = {
-    var x = 0;
-    for(x <- 2 to n){
-      if(n % x == 0) return false;
+   def isPrime(n: Integer): Boolean = {
+    var x = 0
+    if(n == 1) return false
+    if(n == 2 || n == 3) return true
+    for(x <- 2 to n - 1){
+      if(n % x == 0) return false
     }
-    else return true;
+    return true
    }
    
    def main(args: Array[String]): Unit = {
