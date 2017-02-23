77b45a437c398f85fe58eaa44a314c6930a9a258
nopenoway0 <benjamin.gottheil@sjsu.edu>
2017-02-23 01:23:37 -0800

    finished p6

--- text_num_proc/MathLab.scala
+++ text_num_proc/MathLab.scala
@@ -13,6 +13,12 @@ object MathLab{
 		println(dist((1, 1), (0, 0)))
 		println(dist((3, 0), (0, 0)))
 		println(dot((2.0, 3, 4), (2, 2.0, 2)))
+
+		println(isPrime(0))
+		println(isPrime(1))
+		println(isPrime(2))
+		println(isPrime(3))
+		println(isPrime(8))
 	}
 
 	//***********************************
@@ -35,7 +41,26 @@ object MathLab{
 	}
 	//***********************************
 
+	//***********************************
+	// Problem 3
+	//***********************************
 	def dot(v1:(Double, Double, Double), v2:(Double, Double, Double)) = {
 		v1._1*v2._1 + v1._2*v2._2 + v1._3*v2._3
 	}
+	//***********************************
+
+	//***********************************
+	// Problem 6
+	//***********************************
+	def isPrime(in: Int) = {
+		if(in < 0) throw new Exception("Negative Number")
+		var result = true
+		if(in == 0 || in == 1) result = false
+		for( x <- 2 until in){
+			if(in % x == 0) result = false
+		}
+		result
+	}
+	//***********************************
+
 }
