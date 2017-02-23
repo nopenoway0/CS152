6cc4ba6f615908c3aeb47771d78cad1dc9161cf5
nopenoway0 <benjamin.gottheil@sjsu.edu>
2017-02-22 16:11:43 -0800

    updated p1 and finished p2, p3

--- text_num_proc/MathLab.scala
+++ text_num_proc/MathLab.scala
@@ -7,21 +7,35 @@ import scala.math._
 object MathLab{
 
 	def main(args: Array[String]): Unit = {
+		println(solve(2, -2, -4))
 		println(solve(1, 0, 1))
-		print(dist((1,3),(1,3)))
+		println(solve(1, 0, -1))
+		println(dist((1, 1), (0, 0)))
+		println(dist((3, 0), (0, 0)))
+		println(dot((2.0, 3, 4), (2, 2.0, 2)))
 	}
 
 	//***********************************
 	// Problem 1
 	//***********************************
-	def solve(a: Int, b: Int, c: Int) = {
+	def solve(a: Double, b: Double, c: Double) = {
 		val desc = pow(b, 2) - 4*a*c
 		if(desc < 0) None
-		else Some(((-1 * b) + sqrt(desc) / (2 * a), ((-1 * b) - sqrt(desc) / (2 * a))))
+		else Some((((-1 * b) + sqrt(desc)) / (2 * a), (((-1 * b) - sqrt(desc)) / (2 * a))))
 	}
 	//***********************************
 
-	def dist(x1: Int, y1: Int, x2: Int, y2: Int) = {
-		println("holding")
+	//***********************************
+	// Problem 2
+	//***********************************
+	def dist(p1: (Double, Double), p2: (Double, Double)) = {
+		val x_diff = (p1._1 - p2._1)
+		val y_diff = (p1._2 - p2._2)
+		sqrt(pow(x_diff, 2) + pow(y_diff, 2))
+	}
+	//***********************************
+
+	def dot(v1:(Double, Double, Double), v2:(Double, Double, Double)) = {
+		v1._1*v2._1 + v1._2*v2._2 + v1._3*v2._3
 	}
 }
