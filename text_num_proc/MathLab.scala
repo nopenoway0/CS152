0ad4586f4b0aafba2323cb62d1e7e2ba15222112
nopenoway0 <benjamin.gottheil@sjsu.edu>
2017-02-23 22:26:39 -0800

    tidied up code

--- text_num_proc/MathLab.scala
+++ text_num_proc/MathLab.scala
@@ -44,9 +44,7 @@ object MathLab{
 	//***********************************
 	// Problem 3
 	//***********************************
-	def dot(v1:(Double, Double, Double), v2:(Double, Double, Double)) = {
-		v1._1*v2._1 + v1._2*v2._2 + v1._3*v2._3
-	}
+	def dot(v1:(Double, Double, Double), v2:(Double, Double, Double)) = v1._1*v2._1 + v1._2*v2._2 + v1._3*v2._3
 	//***********************************
 
 	//***********************************
