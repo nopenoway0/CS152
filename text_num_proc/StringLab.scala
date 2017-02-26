626991b73f8b88ea9f53fee9cc314860f21b1af6
nopenoway0 <benjamin.gottheil@sjsu.edu>
2017-02-26 00:51:09 -0800

    all problems finished

--- text_num_proc/StringLab.scala
+++ text_num_proc/StringLab.scala
@@ -1,3 +1,5 @@
+import scala.util.Random
+
 object StringLab{
 	def isPal(in:String) = {
 		var tmp = in.trim
@@ -27,12 +29,50 @@ object StringLab{
 		}
 	}
 
-	def main(args:Array[String]) = {
-		println(isPal("rotator"))
-		println(isPal("cat"))
+	def mkWord(in: Int = 11) = {
+		val seed = new Random
+		var word = ""
+		for(x <- 0 until in){
+			word += (seed.nextInt(25) + 97).toChar
+		}
+		word
+	}
 
-		println(isPal2("A man, a plan, a canal, ..Panama?"))
-		println(mkPal("mars"))
-		println(mkPal("3X@#"))
+	def mkSentence(sentence_length: Int = 6) = {
+		val seed = new Random
+		var sentence = ""
+		for(x <- 0 until sentence_length){
+			if(x > 0) sentence += " "
+			sentence += mkWord(seed.nextInt(12) + 1)
+		}
+		sentence.replace(sentence(0).toString, sentence(0).toString.toUpperCase) + "."
+
+	}
+
+	def main(args:Array[String]) = {
+		println("\nisPal Tests")
+		println("rotator:" + isPal("rotator"))
+		println("cat: " + isPal("cat"))
+
+		println("\nisPal2 Tests")
+		println("A man, a plan, a canal, ..Panama?: " + isPal2("A man, a plan, a canal, ..Panama?"))
+		println("In girum imus nocte et consumimur igni: " + isPal2("In girum imus nocte et consumimur igni"))
+		println("\nmkPal Tests")
+		println("mars: " + mkPal("mars"))
+		println("3X@#: " + mkPal("3X@#"))
+		
+		println("\nmkWord Tests")
+		println(mkWord())
+		println(mkWord())
+		println(mkWord())
+		println(mkWord(20))
+
+		println("\nMK Sentence Tests")
+		println(mkSentence())
+		println(mkSentence())
+		println(mkSentence())
+		println(mkSentence())
+		println(mkSentence(5))
+
 	}
 }
