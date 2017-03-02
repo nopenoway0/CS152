219294bbd9fedbe784ec3bde15eb2c1b390d1a5c
nopenoway0 <benjamin.gottheil@sjsu.edu>
2017-03-02 12:15:44 -0800

    finished tail add and mult

--- recursion_lab/lab.scala
+++ recursion_lab/lab.scala
@@ -8,10 +8,29 @@ object lab{
 	
 	def exp2(m: Int): Int = if(m == 1) 2 else mul(2, exp2(dec(m)))
 
+	def hyperExp(m: Int): Int = if(m == 0) 1 else exp2(hyperExp(dec(m)))
+
+	def tailAdd(n: Int, m: Int) = {
+		def helper(count: Int, n: Int):Int = {
+			if(count == 0) n else helper(dec(count), inc(n))
+		}
+		helper(n, m)
+	}
+
+	def tailMul(n: Int, m: Int) = {
+		def helper(n: Int, m: Int): Int = {
+			if(n == 1) m else tailAdd(m, helper(dec(n),m))
+		}
+		helper(n,m)
+	}
+
 	def main(args:Array[String]) ={
 		println(add(4, 5))
 		println(mul(10, 5))
 		println(mul(321, 10))
 		println(exp2(4))
+		//println(hyperExp(4))
+		println(tailAdd(21, 10))
+		println(tailMul(10, 25))
 	}
 }
