150dd7761ae20f03aece8fd0384a34615c4f5dd7
nopenoway0 <benjamin.gottheil@sjsu.edu>
2017-03-07 12:56:52 -0800

    ameoba assign. initial

---
+++ ameoba/ameoba.scala
@@ -0,0 +1,24 @@
+object Processor {
+	var ip = 0
+	var ir = 0
+
+	// Store variable in hash map table
+	def preProcess(Name: String) = {
+		1
+	}
+
+	def fetchExecute() = {
+		()
+	}
+}
+
+class Variable {
+	var content = 0
+}
+
+class Instruction{
+	var opcode = ""
+	var operand1 = ""
+	var operand2 = ""
+	var operand3 = ""
+}
