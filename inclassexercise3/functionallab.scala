4de93a92a05e48d4272c3b1891ce8a86e12365a4
nopenoway0 <benjamin.gottheil@sjsu.edu>
2017-03-17 02:18:04 -0700

    finished p2

--- inclassexercise3/functionallab.scala
+++ inclassexercise3/functionallab.scala
@@ -10,15 +10,22 @@ object functions2{
 	// Problem 2
 	def id[T](x: T) = x
 
-	def selfIter[T, S](f: T=>S, n: Int): T=>T = {
-		if(n == 0) id _ else compose(f _, selfIter(f, n - 1))
+	def selfIter[T](f: T=>T, n: Int): T=>T = {
+		//def fun(in: T) = f(in)
+		if(n == 1) f else compose(f, selfIter(f, n - 1)) 
+		//compose(f, f)
 	}
 
 	// Problem 3
 	
+
+
 	// Test functions p1
 	def len(x: String) = x.length
 	def isEven(x: Int) = x % 2 == 0
+
+	// Test functions p2
+	def addSelf(x: Int) = 2 * x
 
 	// Test functions p2
 	//def in(x: Double) = x + 1
@@ -27,5 +34,8 @@ object functions2{
 	def main(args:Array[String]) = {
 		val hasEven = compose(isEven _, len _)
 		println(hasEven("Bating"))
+
+		val double = selfIter(addSelf _, 1)
+		println(double(2))
 	}
 }
