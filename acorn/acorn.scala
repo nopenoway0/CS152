f9d4519f99a4123afb0713b4b5e8fdfaa0b7184a
nopenoway0 <benjamin.gottheil@sjsu.edu>
2017-04-11 12:54:46 -0700

    uncommented tests

--- acorn/acorn.scala
+++ acorn/acorn.scala
@@ -15,8 +15,8 @@ object testAcorn {
     
   def main(args: Array[String]) {
     execute(Sum(Number(3.1), Number(4.2)))
-    //execute(And(Boole(true), Boole.TRUE))
-    //execute(Sum(Number(3.1), Boole.FALSE))
-    //execute(Sum(Sum(Number(3.1), Number(4.2)), Sum(Number(3.5), Number(2.8))))
+    execute(And(Boole(true), Boole.TRUE))
+    execute(Sum(Number(3.1), Boole.FALSE))
+    execute(Sum(Sum(Number(3.1), Number(4.2)), Sum(Number(3.5), Number(2.8))))
   }
 }
