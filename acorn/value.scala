91ed9f96f6f8c82d945bc286f364c9140b9da916
nopenoway0 <benjamin.gottheil@sjsu.edu>
2017-04-11 12:55:48 -0700

    added Boole class

--- acorn/value.scala
+++ acorn/value.scala
@@ -9,11 +9,22 @@ class Number(val value: Double) extends Literal {
   def *(other: Number) = Number(this.value * other.value)
   def -(other: Number) = Number(this.value - other.value)
   def /(other: Number) = Number(this.value / other.value)
-  //def <(other: Number) = Boole(this.value < other.value)
-  //def ==(other: Number) = Boole(this.value == other.value)
+  def <(other: Number) = Boole(this.value < other.value)
+  def ==(other: Number) = Boole(this.value == other.value)
   override def toString  = value.toString
 }
 
 object Number {
   	def apply(value: Double) = new Number(value)
+}
+
+class Boole(val value: Boolean) extends Literal{
+	def &&(other: Boole) = Boole(this.value && other.value)
+	override def toString = this.value.toString
+}
+
+object Boole{
+	def apply(value: Boolean) = new Boole(value)
+	val TRUE = Boole(true)
+	val FALSE = Boole(false)
 }
